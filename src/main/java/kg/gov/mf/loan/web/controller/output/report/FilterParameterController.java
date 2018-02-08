ed110e565c570999c1158ff6ee4952df3f9f9f51
Marlen Akimaliev <makimaliev@github.com>
2018-02-08 16:17:08 +0600

    Filter Parameter Controller & CRUD

---
+++ src/main/java/kg/gov/mf/loan/web/controller/output/report/FilterParameterController.java
@@ -0,0 +1,103 @@
+package kg.gov.mf.loan.web.controller.output.report;
+
+import kg.gov.mf.loan.output.report.model.FilterParameter;
+import kg.gov.mf.loan.output.report.service.FilterParameterService;
+import kg.gov.mf.loan.output.report.service.ReportTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FilterParameterController {
+	
+	@Autowired
+    private FilterParameterService filterParameterService;
+	
+	
+	@Autowired
+    private ReportTemplateService reportTemplateService;
+	
+     
+    public void setFilterParameterService(FilterParameterService rs)
+    {
+        this.filterParameterService = rs;
+    }
+
+	@RequestMapping(value = "/filterParameter/list", method = RequestMethod.GET)
+	public String listFilterParameters(Model model) {
+		model.addAttribute("filterParameter", new FilterParameter());
+		model.addAttribute("filterParameterList", this.filterParameterService.findAll());
+
+		return "output/report/filterParameterList";
+	}
+	
+	@RequestMapping("filterParameter/{id}/view")
+	public String viewFilterParameterById(@PathVariable("id") long id, Model model) {
+
+		FilterParameter filterParameter = this.filterParameterService.findById(id);
+
+		model.addAttribute("filterParameter", filterParameter);
+
+		return "output/report/filterParameterView";
+	}
+	
+	@RequestMapping("filterParameter/{id}/details")
+	public String viewFilterParameterDetailsById(@PathVariable("id") long id, Model model) {
+
+		FilterParameter filterParameter = this.filterParameterService.findById(id);
+
+		model.addAttribute("filterParameter", filterParameter);
+
+		return "output/report/filterParameterDetails";
+	}	
+    
+	
+	@RequestMapping(value = "/filterParameter/add", method = RequestMethod.GET)
+	public String getFilterParameterAddForm(Model model) {
+
+		model.addAttribute("filterParameter", new FilterParameter());
+
+		return "output/report/filterParameterForm";
+	}
+	
+	@RequestMapping("/filterParameter/{id}/edit")
+	public String getFilterParameterEditForm(@PathVariable("id") long id, Model model) {
+		model.addAttribute("filterParameter", this.filterParameterService.findById(id));
+		return "output/report/filterParameterForm";
+
+	}
+
+	@RequestMapping(value = "/filterParameter/save", method = RequestMethod.POST)
+	public String saveFilterParameterAndRedirectToFilterParameterList(@Validated @ModelAttribute("filterParameter") FilterParameter filterParameter, BindingResult result) {
+
+		if (result.hasErrors()) {
+			System.out.println(" ==== BINDING ERROR ====" + result.getAllErrors().toString());
+		} else if (filterParameter.getId() == 0) {
+			this.filterParameterService.create(filterParameter);
+		} else {
+			this.filterParameterService.edit(filterParameter);
+		}
+
+		return "redirect:/filterParameter/list";
+
+	}
+
+	@RequestMapping("/filterParameter/{id}/remove")
+	public String removeFilterParameterAndRedirectToFilterParameterList(@PathVariable("id") long id) {
+
+		this.filterParameterService.deleteById(id);
+
+		return "redirect:/filterParameter/list";
+	}
+
+     
+
+     
+
+}
