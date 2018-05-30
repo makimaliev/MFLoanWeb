45b95cfea5cac3501b56788d3f58f3e1bdf8b17b
Marlen Akimaliev <makimaliev@github.com>
2018-05-30 13:09:36 +0600

    debtor internationalization

---
+++ src/main/resources/manage_data.sql
@@ -0,0 +1,103 @@
+# debtor
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Debtor', 'Заемщиктер', 'label.debtor.page.title', 'Список заемщиков');
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Search', 'Издоо', 'label.search', 'Поиск');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('First', 'Биринчиси', 'label.pagination.first', 'Начало');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Previous', 'Буга чейинкиси', 'label.pagination.prev', 'Пред');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Next', 'Мындан кийинкиси', 'label.pagination.next', 'След');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Last', 'Акыркысы', 'label.pagination.last', 'Послед');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Page Size', 'Баракча чондугу', 'label.pageSize', 'Кол-во');
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('add Debtor', 'Жаны заемщик', 'label.debtor.addDebtor', 'Добавить заемщика');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('add Loan', 'Жаны кредит', 'label.debtor.addLoan', 'Добавить кредит');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('add Agreement', 'Жаны куроо', 'label.debtor.addAgreement', 'Добавить договор залога');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('add Procedure', 'Жаны ондуруу процедурасы', 'label.debtor.addProcedure', 'Добавить процедуру взыскания');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('add Phase', 'Жаны ондурусу фазасы', 'label.debtor.addPhase', 'Добавить фазу взыскания');
+
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('ID', 'ID', 'label.debtor.table.id', 'ID');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Name', 'Аталышы', 'label.debtor.table.name', 'Наименование');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Debtor Type', 'Заемщик туру', 'label.debtor.table.debtorType', 'Вид заемщика');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('WorkSector', 'Тармак', 'label.debtor.table.workSector', 'Отрасль');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('District', 'Район', 'label.debtor.table.district', 'Район');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('OrgForm', 'Формасы', 'label.debtor.table.orgForm', 'Орг.форма');
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Debtor', 'Заемщик', 'label.add.debtor.title', 'Заемщик');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Name', 'Аталышы', 'label.add.debtor.name', 'Наименование');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Type', 'Туру', 'label.add.debtor.type', 'Вид');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Organizational Form', 'Формасы', 'label.add.debtor.orgForm', 'Орг.форма');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('WorkSector', 'Тармагы', 'label.add.debtor.workSector', 'Отрасль');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Owner', 'заемщик', 'label.add.debtor.owner', 'Заемщик');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Save', 'Сактоо', 'label.add.debtor.save', 'Сохранить');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Cancel', 'Жокко чыгаруу', 'label.add.debtor.cancel', 'Отменить');
+
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Loans', 'Кредит маалыматы', 'label.debtor.tab.loans', 'Кредитная информация');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Collateral', 'Куроо маалыматы', 'label.debtor.tab.agreements', 'Залоговое обеспечение');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Collection', 'Ондуруу маалыматтары', 'label.debtor.tab.procedures', 'Принудительное взыскание');
+
+
+
+
+
+# loan
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Loan', 'Насыя', 'label.add.loan.title', 'Кредит');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Save', 'Сактоо', 'label.add.loan.save', 'Сохранить');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Cancel', 'Жокко чыгаруу', 'label.add.loan.cancel', 'Отменить');
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Select Currency', 'Валюта танданыз', 'label.add.loan.selectCurrency', 'Выбрать валюту');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Select Type', 'Насыя турун танданыз', 'label.add.loan.selectType', 'Выбрать вид ');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Select State', 'Статусун танданыз', 'label.add.loan.selectState', 'Выбрать статус');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Select Parent', 'Башкы насыясын танданыз', 'label.add.loan.selectParent', 'Выбрать основной кредит');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Select Order', 'Насыя беруу чечимин танданыз', 'label.add.loan.selectCreditOrder', 'Выбрать решение о выдаче кредита');
+
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Registration Number', 'Каттоо номуру', 'label.loan.regNumber', 'Регистрационный номер');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Registration Date', 'Каттоо датасы', 'label.loan.regDate', 'Дата регистрации');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Amount', 'Суммасы', 'label.loan.amount', 'Сумма по договору');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Currency', 'Валютасы', 'label.loan.currency', 'Валюта');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Type', 'Туру', 'label.loan.type', 'Вид');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('State', 'Статусу', 'label.loan.state', 'Статус');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Supervisor', 'Куратору', 'label.loan.supervisorId', 'Куратор');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Parent', 'Башкысы', 'label.loan.parent', 'Основной кредит');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Order', 'Насыя беруу чечими', 'label.loan.creditOrderId', 'Решение о выдаче кредита');
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Registration Number', 'Каттоо номуру', 'label.loan.table.regNumber', 'Регистрационный номер');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Registration Date', 'Каттоо датасы', 'label.loan.table.regDate', 'Дата регистрации');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Amount', 'Суммасы', 'label.loan.table.amount', 'Сумма по договору');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Currency', 'Валютасы', 'label.loan.table.currency', 'Валюта');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Type', 'Туру', 'label.loan.table.type', 'Вид');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('State', 'Статусу', 'label.loan.table.state', 'Статус');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Supervisor', 'Куратору', 'label.loan.table.superId', 'Куратор');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Parent', 'Башкысы', 'label.loan.table.parentId', 'Основной кредит');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Order', 'Насыя беруу чечими', 'label.loan.table.creditOrderId', 'Решение о выдаче кредита');
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Has SubLoan', 'Субкредити бар', 'label.loan.table.hasSubLoan', 'Наличие субкредитов');
+
+
+
+
+# collateral
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Number', 'Номери', 'label.agreement.table.number', 'Номер');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Date', 'Датасы', 'label.agreement.table.date', 'Дата');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Reg number', 'Каттоо номери', 'label.agreement.table.collRegNumber', 'Регистрационный номер');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Reg Date', 'Каттоо датасы', 'label.agreement.table.collRegDate', 'Дата регистрации');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Notary reg number', 'Нотариус каттоо номери', 'label.agreement.table.notaryRegNumber', 'Номер нотариальной регистрации');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Notary reg date', 'Нотариус каттоо датасы', 'label.agreement.table.notaryRegDate', 'Дата нотариальной регистрации');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Arrest reg number', 'Арест каттоо номери', 'label.agreement.table.arrestRegNumber', 'Номер ареста');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Arrest reg date', 'Арест каттоо датасы', 'label.agreement.table.arrestRegDate', 'Дата ареста');
+
+
+# collection
+
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('ID', 'ID', 'label.procedure.table.id', 'ID');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('ID', 'ID', 'label.table.id', 'ID');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Start date', 'Башталган датасы', 'label.procedure.table.startDate', 'Дата инициирования');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Close date', 'Буткон датасы', 'label.procedure.table.closeDate', 'Дата завершения');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Last Phase', 'Акыркы фазасы', 'label.procedure.table.lastPhase', 'Последняя фаза');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Last status', 'Акыркы статусы', 'label.procedure.table.lastStatusId', 'Последний статус');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Status', 'Статусу', 'label.procedure.table.status', 'Статус');
+INSERT INTO `mfloan`.`message_resource` (`eng`, `kgz`, `messageKey`, `rus`) VALUES ('Type', 'Туру', 'label.procedure.table.type', 'Вид');
