ea9d90661ca01d1ee5d72560b589954967f94fd6
makimaliev <makimaliev@gmail.com>
2018-10-11 17:19:28 +0600

    Eridan organization list search,sort

--- src/main/java/kg/gov/mf/loan/web/controller/admin/org/RestOrganizationController.java
+++ src/main/java/kg/gov/mf/loan/web/controller/admin/org/RestOrganizationController.java
@@ -1,6 +1,4 @@
 package kg.gov.mf.loan.web.controller.admin.org;
-
-
 import kg.gov.mf.loan.admin.org.model.Organization;
 import kg.gov.mf.loan.admin.org.repository.OrganizationRepository;
 import kg.gov.mf.loan.web.exception.ResourceNotFoundExcption;
@@ -8,40 +6,29 @@ import kg.gov.mf.loan.web.fetchModels.ContactListModel;
 import kg.gov.mf.loan.web.fetchModels.OrganizationMetaModel;
 import kg.gov.mf.loan.web.fetchModels.OrganizationModel;
 import kg.gov.mf.loan.web.util.Meta;
+import org.hibernate.jpa.criteria.ValueHandlerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.validation.Valid;
 import java.math.BigInteger;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static javax.print.attribute.standard.MediaSizeName.A;
 
 @RestController
 @RequestMapping("/api")
 public class RestOrganizationController {
-
     @Autowired
     OrganizationRepository organizationRepository;
-
     /** The entity manager. */
     @PersistenceContext
     private EntityManager entityManager;
 
-//    @GetMapping("/organizations/search")
-//    public String[] getOrganizationsByName(@RequestParam(value = "s" )String s){
-//        List<Organization> organizations=organizationRepository.findByName(s);
-//        String[] sOrganizations=new String[organizations.size()];
-//        int i=-1;
-//        for (Organization organization:organizations){
-//            sOrganizations[i++]="["+organization.getId()+"]"+organization.getName();
-//        }
-//        return sOrganizations;
-//    }
 
 
     @PostMapping("/organizations")
@@ -56,13 +43,11 @@ public class RestOrganizationController {
         Integer perPage = Integer.parseInt(perPageStr);
         Integer offset = (page-1)*perPage;
 
-        boolean searchByDistrict= datatable.containsKey("datatable[query][districtId]");
+        boolean searchBy = datatable.containsKey("datatable[query][generalSearch]");
+        String searchStr = searchBy?datatable.get("datatable[query][generalSearch]"):null;
+
+        boolean searchByDistrict = datatable.containsKey("datatable[query][districtId]");
         String districtStr = searchByDistrict?datatable.get("datatable[query][districtId]"):null;
-
-        boolean generalSearch= datatable.containsKey("datatable[query][generalSearch]");
-        String searchStr = generalSearch?datatable.get("datatable[query][generalSearch]"):null;
-
-
 
 
         String baseQuery="SELECT o.id as id,o.name as organizationName, o.description as organizationDescription , o.address_id as addressId," +
@@ -70,32 +55,101 @@ public class RestOrganizationController {
                 "from organization o, address a, district d,identity_doc i " +
                 "where a.id=o.address_id " +
                 "and d.id=a.district_id " +
-                "and i.id=o.identity_doc_id "+
-                getDistrictQuery(searchByDistrict,districtStr)+
-                getSearchQuery(generalSearch,searchStr)+
-                " order by " + sortField + " " + sortStr + " LIMIT " + offset +"," + perPage;
-
-        String countQuery="Select count(1) " +
-                "from organization o, address a, district d,identity_doc i " +
-                "where a.id=o.address_id " +
-                "and d.id=a.district_id " +
                 "and i.id=o.identity_doc_id " +
                 getDistrictQuery(searchByDistrict,districtStr)+
-                getSearchQuery(generalSearch,searchStr);
-        BigInteger count = (BigInteger)entityManager.createNativeQuery(countQuery).getResultList().get(0);
+                " order by " + sortField + " " + sortStr;
         Query query=entityManager.createNativeQuery(baseQuery,OrganizationModel.class);
         List<OrganizationModel> organizations =query.getResultList();
+
+        OrganizationMetaModel metaModel=new OrganizationMetaModel();
+
         setContactList(organizations);
-        setBankDataOrganization(organizations);
+//        setBankDataOrganization(organizations);
         setStaffName(organizations);
-        OrganizationMetaModel organizationMetaModel=new OrganizationMetaModel();
+        List<OrganizationModel> organizationModels=new ArrayList<>();
+        if(searchBy){
+            organizationModels=searchFunction(organizations,searchStr);
+        }
+        else{
+            organizationModels=organizations;
+        }
+        BigInteger count=BigInteger.valueOf(organizationModels.size());
         Meta meta=new Meta(page, count.divide(BigInteger.valueOf(perPage)), perPage, count, sortStr, sortField);
-        organizationMetaModel.setMeta(meta);
-        organizationMetaModel.setData(organizations);
-        return organizationMetaModel;
+        metaModel.setMeta(meta);
+//        organizationModels=sortFunction(organizationModels,sortStr,sortField);
+        List<OrganizationModel> smallList=new ArrayList<>();
+        for(int i=offset;i<offset+perPage;i++){
+            try {
+                smallList.add(organizationModels.get(i));
+            }
+            catch (Exception e){
+                break;
+            }
+        }
+        metaModel.setData(smallList);
+        return metaModel;
     }
 
+    public List<OrganizationModel> sortFunction(List<OrganizationModel> organizations,String sortStr,String sortField){
+        int key=0;
+        System.out.println("=======sortsssssstr=========================================================================================================================");
+        System.out.println(sortStr);
+        System.out.println("================================================================================================================================");
+        String alphabet="0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        if(sortField.equals("organizationName")){
+            key=1;
+            while(key<organizations.size()){
+                List<OrganizationModel> smallList=new ArrayList<>();
+                for(int i=0;i<key;i++){
+                    smallList.add(organizations.get(i));
+                }
+                while(smallList.size()!=0){
+                    String a=smallList.get(smallList.size()/2).getOrganizationName().toLowerCase();
+                    a=a.replaceAll("\"","");
+                    String b=organizations.get(key).getOrganizationName().toLowerCase();
+                    b=b.replaceAll("\"","");
+                    int compare=a.compareTo(b);
+                    if(compare>0){
+                        for(int j=smallList.size()/2;j<smallList.size();j++){
+                            smallList.remove(j);
+                        }
+                        if(smallList.size()==1){
+                            if(organizations.indexOf(smallList.get(0))-1==-1){
+                                organizations.add(0,organizations.get(key));
+                                OrganizationModel o=organizations.get(organizations.indexOf(smallList.get(0)));
+                                organizations.remove(o);
+                                organizations.add(0,o);
+                            }
+                            else{
+                                organizations.add(organizations.indexOf(smallList.get(0))-1,organizations.get(key));
+                            }
+                            organizations.remove(key);
+                            break;
+                        }
+                    }
+                    else if(compare<0){
+                        for(int j=0;j<smallList.size()/2;j++){
+                            smallList.remove(j);
+                        }
+                        if(smallList.size()==1){
+                            organizations.add(organizations.indexOf(smallList.get(0)),organizations.get(key));
+                            organizations.remove(key);
+                            break;
+                        }
+                    }
+                    else{
+                        organizations.add(organizations.indexOf(smallList.get(smallList.size()/2)),organizations.get(key));
+                        organizations.remove(key);
+                        break;
+                    }
 
+                }
+                key++;
+            }
+        }
+
+        return organizations;
+    }
     private String getDistrictQuery(boolean searchByDistrict, String districtStr){
         if(searchByDistrict)
             return "AND a.district_id = " + districtStr + " \n";
@@ -103,16 +157,16 @@ public class RestOrganizationController {
             return "";
     }
 
-    private String getSearchQuery(boolean generalSearch, String searchStr){
-        if(generalSearch)
-            return "AND ( a.line like '%"+searchStr+"%'\n" +
-                    "      or i.pin  like '%"+searchStr+"%'\n" +
-                    "      or i.number like '%"+searchStr+"%'\n" +
-                    "      or o.name like '%"+searchStr+"%'\n" +
-                    "      or o.description like '%"+searchStr+"%'\n" +
-                    "      or d.name like '%"+searchStr+"%') ";
-        else
-            return "";
+    public List<OrganizationModel> searchFunction(List<OrganizationModel> organizations,String searchStr){
+        searchStr=searchStr.toLowerCase();
+        List<OrganizationModel> smallList=new ArrayList<>();
+        for(int i=0;i<organizations.size();i++){
+            if(organizations.get(i).getBigData().toLowerCase().contains(searchStr) || organizations.get(i).getDistrictName().toLowerCase().contains(searchStr)|| organizations.get(i).getOrganizationDescription().toLowerCase().contains(searchStr)|| organizations.get(i).getOrganizationName().toLowerCase().contains(searchStr)){
+                smallList.add(organizations.get(i));
+            }
+        }
+
+        return smallList;
     }
 
     private void setSmallData(String tableName,String column,String tableOrgId,List<OrganizationModel> organizations){
@@ -144,7 +198,6 @@ public class RestOrganizationController {
         }
     }
     private void setStaffName(List<OrganizationModel> organizations){
-
         String baseQuery="select s.organization_id as id, s.name as smallData \n" +
                 "from organization o, staff s \n" +
                 "where s.organization_id = o.id";
@@ -153,14 +206,12 @@ public class RestOrganizationController {
 //        System.out.println("====================================================================================================");
         for(OrganizationModel i :organizations){
             for(ContactListModel j:staffs){
-                if(i.getId()==j.getId() && j.getSmallData()!=null){
-//                    System.out.println(j.getSmallData()+":"+i.getOrganizationName());
+                if(i.getId()==j.getId() && j.getSmallData()!=null&& !i.getBigData().contains(j.getSmallData())){
                     i.setBigData(i.getBigData()+ " " + j.getSmallData());
                 }
             }
         }
 //        System.out.println("====================================================================================================");
-
     }
     private void setContactList(List<OrganizationModel> organizations){
         String baseQuery="select o.id as id, c.name as smallData \n" +
@@ -174,10 +225,8 @@ public class RestOrganizationController {
                     i.setBigData(i.getBigData()+ " " + j.getSmallData());
                 }
             }
-
         }
     }
-
     @DeleteMapping("/organizations/{orgId}")
     public ResponseEntity<?> deleteOrganization(@PathVariable(value = "orgId") long orgId){
         Organization organization=organizationRepository.findOne(orgId);
@@ -186,15 +235,12 @@ public class RestOrganizationController {
         organizationRepository.delete(organization);
         return ResponseEntity.ok().build();
     }
-
     @PutMapping("/organizations/{orgId}")
     public Organization updateOrganization(
             @PathVariable (value = "orgId") long orgId, @Valid @RequestBody Organization orgRequest){
         Organization organization=organizationRepository.findOne(orgId);
         if(organization == null)
             throw new ResourceNotFoundExcption("OrganizationId " + orgId + " not found");
-
         return organization;
     }
-
 }
