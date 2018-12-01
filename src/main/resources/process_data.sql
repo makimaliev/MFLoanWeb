83ab8fb5f993dd470a51057bdf1de4b80bcce37a
Marlen Akimaliev <makimaliev@gmail.com>
2018-12-01 15:54:52 +0600

    loan procs calculate libor fix

--- src/main/resources/process_data.sql
+++ src/main/resources/process_data.sql
@@ -180,7 +180,7 @@ BEGIN
       IF dIYMethod != 1 THEN SET nOD = 360;
       END IF;
 
-      SET daysInPer = DATEDIFF(curDate, prevDate);
+      SET daysInPer = calculateDays(prevDate, curDate, dIYMethod);
 
       SET total = total + val*prevRate*daysInPer/nOD/100;
 
@@ -192,7 +192,7 @@ BEGIN
 
     CLOSE tCursor;
 
-    SET daysInPer = DATEDIFF(toDate, prevDate);
+    SET daysInPer = calculateDays(prevDate, toDate, dIYMethod);
 
     SET total = total + val*tRate*daysInPer/nOD/100;
 
@@ -286,7 +286,7 @@ BEGIN
       IF dIYMethod != 1 THEN SET nOD = 360;
       END IF;
 
-      SET daysInPer = DATEDIFF(curDate, prevDate);
+      SET daysInPer = calculateDays(prevDate, curDate, dIYMethod);
 
       SET total = total + val*prevRate*daysInPer/nOD/100;
 
@@ -298,7 +298,7 @@ BEGIN
 
     CLOSE tCursor;
 
-    SET daysInPer = DATEDIFF(toDate, prevDate);
+    SET daysInPer = calculateDays(prevDate, toDate, dIYMethod);
 
     SET total = total + val*tRate*daysInPer/nOD/100;
 
@@ -392,7 +392,7 @@ BEGIN
       IF dIYMethod != 1 THEN SET nOD = 360;
       END IF;
 
-      SET daysInPer = DATEDIFF(curDate, prevDate);
+      SET daysInPer = calculateDays(prevDate, curDate, dIYMethod);
 
       SET total = total + val*prevRate*daysInPer/nOD/100;
 
@@ -404,7 +404,7 @@ BEGIN
 
     CLOSE tCursor;
 
-    SET daysInPer = DATEDIFF(toDate, prevDate);
+    SET daysInPer = calculateDays(prevDate, toDate, dIYMethod);
 
     SET total = total + val*tRate*daysInPer/nOD/100;
 
@@ -694,25 +694,6 @@ BEGIN
     RETURN dIYMethod;
 
   END ;;
-
-
-/*!50003 DROP FUNCTION IF EXISTS `getTransactionOrder` */;
-CREATE DEFINER=`root`@`localhost` FUNCTION `getTransactionOrder`(inDate date, loan_id bigint) RETURNS int(11)
-  BEGIN
-
-    DECLARE transactionOrder INT DEFAULT 1;
-
-    SELECT term.transactionOrderId INTO transactionOrder
-    FROM creditTerm term
-    WHERE term.loanId = loan_id
-          AND term.startDate < inDate
-          AND term.record_status = 1
-    ORDER BY term.startDate DESC LIMIT 1;
-
-    RETURN transactionOrder;
-
-  END ;;
-
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -765,6 +746,36 @@ BEGIN
     order by startDate desc limit 1;
 
     RETURN penalty_limit;
+
+  END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `getTransactionOrder` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `getTransactionOrder`(inDate date, loan_id bigint) RETURNS int(11)
+BEGIN
+
+    DECLARE transactionOrder INT DEFAULT 1;
+
+    SELECT term.transactionOrderId INTO transactionOrder
+    FROM creditTerm term
+    WHERE term.loanId = loan_id
+          AND term.startDate < inDate
+          AND term.record_status = 1
+    ORDER BY term.startDate DESC LIMIT 1;
+
+    RETURN transactionOrder;
 
   END ;;
 DELIMITER ;
@@ -2785,4 +2796,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-01 11:55:56
+-- Dump completed on 2018-12-01 15:52:06
