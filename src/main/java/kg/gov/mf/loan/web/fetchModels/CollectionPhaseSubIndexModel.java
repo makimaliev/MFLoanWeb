8d78b550a8b3be1989e17a1364648c0dec40f2d0
Eridan123 <saryguloveridan@gmail.com>
2019-01-11 15:07:16 +0600

    collectionPhaseSubIndexModel added, doc index button set security,sidebar security credit order change

---
+++ src/main/java/kg/gov/mf/loan/web/fetchModels/CollectionPhaseSubIndexModel.java
@@ -0,0 +1,64 @@
+package kg.gov.mf.loan.web.fetchModels;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CollectionPhaseSubIndexModel {
+
+    @Id
+    private Long id;
+    private String name;
+    private Long indexId;
+    private String indexName;
+    private Long groupId;
+    private String groupName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getIndexId() {
+        return indexId;
+    }
+
+    public void setIndexId(Long indexId) {
+        this.indexId = indexId;
+    }
+
+    public String getIndexName() {
+        return indexName;
+    }
+
+    public void setIndexName(String indexName) {
+        this.indexName = indexName;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
