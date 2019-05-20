94783ef9baf958b1ec1a9442ec25ff792f8fde3a
Eridan123 <saryguloveridan@gmail.com>
2019-05-20 16:21:15 +0600

    second collectionPhaseView

--- src/main/java/kg/gov/mf/loan/web/controller/manage/CollectionPhaseViewController.java
+++ src/main/java/kg/gov/mf/loan/web/controller/manage/CollectionPhaseViewController.java
@@ -4,13 +4,13 @@ import kg.gov.mf.loan.admin.org.service.DistrictService;
 import kg.gov.mf.loan.admin.org.service.RegionService;
 import kg.gov.mf.loan.manage.service.collection.PhaseStatusService;
 import kg.gov.mf.loan.manage.service.collection.PhaseTypeService;
-import kg.gov.mf.loan.manage.service.collection.ProcedureStatusService;
 import kg.gov.mf.loan.output.report.model.ReferenceView;
 import kg.gov.mf.loan.output.report.service.ReferenceViewService;
 import kg.gov.mf.loan.web.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -50,11 +50,46 @@ public class CollectionPhaseViewController {
 
         model.addAttribute("regions",regionService.findAll());
         model.addAttribute("districts",districtService.findAll());
-        model.addAttribute("phaseTypes",phaseTypeService.list());
+        model.addAttribute("phaseTypes",phaseTypeService.getById(13L));
         model.addAttribute("procedures",phaseStatusService.list());
 
         model.addAttribute("loggedinuser", Utils.getPrincipal());
 
         return "manage/debtor/collectionprocedure/collectionphase/collectionPhaseViewList";
     }
+
+    @RequestMapping("/collectionPhaseViews/second")
+    public String collectionPhaseViewList1(ModelMap model){
+
+        List<ReferenceView> collectionPhaseGroups=referenceViewService.findByParameter("collection_phase_group");
+        model.addAttribute("collectionPhaseGroups",collectionPhaseGroups);
+
+        List<ReferenceView> collectionPhaseIndexes=referenceViewService.findByParameter("collection_phase_index");
+        model.addAttribute("collectionPhaseIndexes",collectionPhaseIndexes);
+
+        List<ReferenceView> collectionPhaseSubIndexes=referenceViewService.findByParameter("collection_phase_sub_index");
+        model.addAttribute("collectionPhaseSubIndexes",collectionPhaseSubIndexes);
+
+        List<ReferenceView> finGroups=referenceViewService.findByParameter("fin_group");
+        model.addAttribute("finGroups",finGroups);
+
+        model.addAttribute("regions",regionService.findAll());
+        model.addAttribute("districts",districtService.findAll());
+        model.addAttribute("phaseTypes",phaseTypeService.getById(13L));
+        model.addAttribute("procedures",phaseStatusService.list());
+
+        model.addAttribute("loggedinuser", Utils.getPrincipal());
+
+        return "manage/debtor/collectionprocedure/collectionphase/collectionPhaseViewList1";
+    }
+
+
+    @RequestMapping("/collectionPhase/{id}/details")
+    public String getDetails(ModelMap model, @PathVariable("id") Long id){
+        model.addAttribute("som",id);
+
+        return  "/manage/debtor/collectionprocedure/collectionphase/collectionPhaseViewDetails";
+
+    }
+
 }
