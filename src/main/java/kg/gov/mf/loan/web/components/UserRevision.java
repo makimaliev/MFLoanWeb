bd66de191a13971745cc2c50b828fe834bf83fd4
Eridan123 <saryguloveridan@gmail.com>
2019-07-20 15:26:38 +0600

    revisions save usernames ot ids

--- src/main/java/kg/gov/mf/loan/web/components/UserRevision.java
+++ src/main/java/kg/gov/mf/loan/web/components/UserRevision.java
@@ -10,13 +10,13 @@ import javax.persistence.Table;
 @Table(name = "user_revisions")
 @RevisionEntity(UserRevisionListener.class)
 public class UserRevision extends DefaultRevisionEntity {
-    private Long user_id;
+    private String username;
 
-    public Long getUser_id() {
-        return user_id;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
--- src/main/java/kg/gov/mf/loan/web/components/UserRevisionListener.java
+++ src/main/java/kg/gov/mf/loan/web/components/UserRevisionListener.java
@@ -1,17 +1,11 @@
 package kg.gov.mf.loan.web.components;
 
-import kg.gov.mf.loan.admin.sys.model.User;
-import kg.gov.mf.loan.admin.sys.service.UserService;
 import org.hibernate.envers.RevisionListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 public class UserRevisionListener implements RevisionListener {
-
-    @Autowired
-    UserService userService;
 
     @Override
     public void newRevision(Object revisionEntity) {
@@ -19,7 +13,6 @@ public class UserRevisionListener implements RevisionListener {
 
         UserRevision userRevision = (UserRevision) revisionEntity;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user=userService.findByUsername(authentication.getName());
-        userRevision.setUser_id(user.getId());
+        userRevision.setUsername(authentication.getName());
     }
 }
