aca482247878f66bb55848819425eeb59b42ec33
Eridan123 <saryguloveridan@gmail.com>
2019-12-11 16:29:20 +0600

    js files

---
+++ WebContent/static/js/data-debtor-agreements2.js
@@ -0,0 +1,219 @@
+//== Class definition
+
+var DatatableDataLocalAgreements = function () {
+    //== Private functions
+
+    // demo initializer
+    var mainTableInit = function () {
+
+        var dataJSONArray = JSON.parse(jsonAgreements);
+
+        var datatable = $('#agreementsTable').mDatatable({
+            // datasource definition
+            data: {
+                type: 'local',
+                source: dataJSONArray
+            },
+
+            // layout definition
+            layout: {
+                theme: 'default', // datatable theme
+                class: '', // custom wrapper class
+                scroll: false, // enable/disable datatable scroll both horizontal and vertical when needed.
+                height: 450, // datatable's body's fixed height
+                footer: false // display/hide footer
+            },
+
+            // column sorting(refer to Kendo UI)
+            sortable: false,
+
+            // column based filtering(refer to Kendo UI)
+            filterable: false,
+
+            pagination: false,
+
+            // inline and bactch editing(cooming soon)
+            // editable: false,
+
+            // columns definition
+            columns: [{
+                field: "id",
+                title: "#",
+                responsive: {hidden: 'xl'},
+            }, {
+                field: "View",
+                width: 80,
+                textAlign: 'center',
+                title: "Просмотр",
+                selector: false,
+                overflow: 'visible',
+                template: function (row) {
+                    return '\
+						<a href="/manage/debtor/'+ debtorId + '/collateralagreement/' + row.id + '/collateralitem/' + row.itemId + '/view" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="View "><i class="la la-arrow-circle-o-right"></i></a>\
+					';
+                }
+            }, {
+                field: "agreementNumber",
+                title: "Номер",
+                template: function (row) {
+                    var agrNum = row.agreementNumber;
+                    if (agrNum == null || agrNum == '')
+                        agrNum = 'б/н';
+
+                    return '\
+						<a href="/manage/debtor/'+ debtorId + '/collateralagreement/' + row.id + '/view">'+agrNum+'</a>\
+					';
+                }
+            },{
+                field: "num_of_loans",
+                title: "Кредиты(Остаток)",
+                width: 150,
+                template: function (row) {
+
+                    if (row.remaining != -777.0){
+                        return '\
+						    <a href="/manage/debtor/'+ debtorId + '/collateralagreement/' + row.id + '/view">'+row.num_of_loans+' ('+(row.remaining).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace(".", ",")+')</a>\
+					    ';
+                    }
+                    else{
+                        return '\
+						    <a href="/manage/debtor/'+ debtorId + '/collateralagreement/' + row.id + '/view">'+row.num_of_loans+' (нет данных)</a>\
+					    ';
+                    }
+                }
+            }, {
+                field: "agreementDate",
+                title: "Дата"
+            }, {
+                field: "itemName",
+                title: "Наименование",
+                width: 450,
+                template: function (row) {
+                    var result = '';
+
+                    if(row.itemName.length <= 50)
+                        result = row.itemName;
+                    else {
+                        result = row.itemName.substr(0, 50);
+                        result = result + '...';
+                    }
+
+                    return result;
+                }
+            }, {
+                field: "itemTypeName",
+                title: "Вид"
+            }, {
+                field: "quantityNameValue",
+                title: "Кол-во",
+                width: 70,
+                template: function (row) {
+                    return row.quantity + ' ' + row.quantityTypeName;
+                }
+            }, {
+                field: "collateralValue",
+                title: "Стоимость",
+                template: function (row) {
+                    return (row.collateralValue).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace(".", ",");
+                }
+            },{
+                field: "status",
+                title: "Статус",
+                template: function (row) {
+
+                    if(row.status!=2){
+                        return '<span class="m-badge m-badge--success m-badge--wide"> Действует</span>';
+                    }
+                    else{
+                        return '<span class="m-badge m-badge--danger m-badge--wide">Снят</span>';
+                    }
+                }
+            }, {
+                field: "action",
+                width: 50,
+                title: "",
+                sortable: false,
+                template: function (row) {
+
+                    var result = '';
+
+                    result = result + '\
+						<a sec:authorize="hasAnyAuthority(ADMIN,PERM_UPDATE_COLLATERALAGREEMENT)" href="/manage/debtor/'+ debtorId + '/collateralagreement/' + row.id +'/save" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="Редактировать">\
+							<i class="la la-edit"></i>\
+						</a>\
+						<a hidden="hidden" href="/manage/debtor/'+ debtorId + '/collateralagreement/' + row.id +'/delete" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="Удалить">\
+							<i class="la la-trash"></i>\
+						</a>\
+					';
+
+                    if(!hasRoleAdmin)
+                        result = '';
+
+                    return result;
+                }
+            }],
+            translate: {
+                records: {
+                    processing: 'Подождите пожалуйста...',
+                    noRecords: 'Записей не найдено'
+                },
+                toolbar: {
+                    pagination: {
+                        items: {
+                            default: {
+                                first: 'Первая страница',
+                                prev: 'Предыдущая страница',
+                                next: 'Следующая страница',
+                                last: 'Последняя страница',
+                                more: 'Другие страницы',
+                                input: 'Номер страницы',
+                                select: 'Выбрать размер страницы'
+                            },
+                            info: 'Отображение {{start}} - {{end}} из {{total}} записей'
+                        }
+                    }
+                }
+            },
+
+            toolbar: {
+                items: {
+                    // pagination
+                    pagination: {
+                        // page size select
+                        pageSizeSelect: [5, 10, 20, 50, 100]
+                    }
+                }
+            }
+        });
+
+        var query = datatable.getDataSourceQuery();
+
+        $('#m_form_search1').on('keyup', function (e) {
+            datatable.search($(this).val().toLowerCase());
+        }).val(query.generalSearch);
+
+        /*
+        $('#m_form_status').on('change', function () {
+            datatable.search($(this).val(), 'Status');
+        }).val(typeof query.Status !== 'undefined' ? query.Status : '');
+
+        $('#m_form_type').on('change', function () {
+            datatable.search($(this).val(), 'Type');
+        }).val(typeof query.Type !== 'undefined' ? query.Type : '');
+
+        $('#m_form_status, #m_form_type').selectpicker();
+        */
+    };
+
+    return {
+        //== Public functions
+        init: function () {
+            // init dmeo
+            mainTableInit();
+        }
+    };
+}();
+
+jQuery(document).ready(function () {
+    DatatableDataLocalAgreements.init();
+});
---
+++ WebContent/static/js/data-debtor-loan-payments1.js
@@ -0,0 +1,214 @@
+//== Class definition
+
+var DatatableDataLocalPayments = function () {
+    //== Private functions
+
+    // demo initializer
+    var mainTableInit = function () {
+
+        var dataJSONArray = JSON.parse(jsonPayments);
+
+        var datatable = $('#paymentsTable').mDatatable({
+            // datasource definition
+            data: {
+                type: 'local',
+                source: dataJSONArray
+            },
+
+            // layout definition
+            layout: {
+                theme: 'default', // datatable theme
+                class: '', // custom wrapper class
+                scroll: false, // enable/disable datatable scroll both horizontal and vertical when needed.
+                height: 450, // datatable's body's fixed height
+                footer: false // display/hide footer
+            },
+
+            // column sorting(refer to Kendo UI)
+            sortable: true,
+
+            // column based filtering(refer to Kendo UI)
+            filterable: false,
+
+            pagination: true,
+
+            // inline and bactch editing(cooming soon)
+            // editable: false,
+
+            // columns definition
+            columns: [{
+                field: "id",
+                title: "#",
+                width: 20,
+                template: function (row) {
+                    return '\
+						<a href="/manage/debtor/'+debtorId+'/loan/'+loanId+'/payment/'+row.id+'/view" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="View "><i class="la la-arrow-circle-o-right"></i></a>\
+					';
+                }
+            }, {
+                field: "paymentDate",
+                title: "Дата",
+                width: 100
+            }, {
+                field: "number",
+                title: "Номер",
+                width: 200
+            }, {
+                field: "totalAmount",
+                title: "Итого",
+                width: 100,
+                template: function (row) {
+                    return (row.totalAmount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace(".", ",");
+                }
+            }, {
+                field: "principal",
+                title: "Осн.сумма",
+                width: 100,
+                template: function (row) {
+                    return (row.principal).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace(".", ",");
+                }
+            }, {
+                field: "interest",
+                title: "Проценты",
+                width: 100,
+                template: function (row) {
+                    return (row.interest).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace(".", ",");
+                }
+            }, {
+                field: "penalty",
+                title: "Штрафы",
+                width: 100,
+                template: function (row) {
+                    return (row.penalty).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace(".", ",");
+                }
+            }, /*{
+                field: "fee",
+                title: "Комиссия",
+                template: function (row) {
+                    return (row.fee).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace(".", ",");
+                }
+            }, */{
+                field: "paymentTypeName",
+                title: "Вид платежа",
+                width: 200
+            }, {
+                field: "record_status",
+                title: "Статус",
+                width: 100,
+                template: function (row) {
+
+                    if(row.record_status==2){
+                        return '<span class="m-badge m-badge--danger m-badge--wide"> Отменен</span>';
+                    }
+                    else{
+                        return '<span class="m-badge m-badge--success m-badge--wide">Действует</span>';
+                    }
+                }
+            }, {
+                field: "action",
+                width: 110,
+                title: "",
+                sortable: false,
+                template: function (row) {
+
+                    var result = '';
+
+                    if(isEmptyChild==true){
+                        if(finGroupId!=9){
+                            if(loggedInUserId==supervisorId || loggedInUserId==1){
+                                result = result + '\
+                            <a sec:authorize="hasAnyAuthority(ADMIN,PERM_UPDATE_PAYMENT)" href="/manage/debtor/'+ debtorId + '/loan/'+ loanId + '/payment/' + row.id +'/save" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="Редактировать">\
+                                <i class="la la-edit"></i>\
+                            </a>\
+                            <a hidden="hidden" href="/manage/debtor/'+ debtorId + '/loan/'+ loanId + '/payment/' + row.id +'/delete" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="Удалить">\
+                                <i class="la la-trash"></i>\
+                            </a>\
+                        ';
+                            }
+                        }
+                        else {
+                            if (loggedInUserId == 1) {
+                                result = result + '\
+                            <a sec:authorize="hasAnyAuthority(ADMIN,PERM_UPDATE_PAYMENT)" href="/manage/debtor/' + debtorId + '/loan/' + loanId + '/payment/' + row.id + '/save" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="Редактировать">\
+                                <i class="la la-edit"></i>\
+                            </a>\
+                            <a hidden="hidden" href="/manage/debtor/' + debtorId + '/loan/' + loanId + '/payment/' + row.id + '/delete" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="Удалить">\
+                                <i class="la la-trash"></i>\
+                            </a>\
+                        ';
+                            }
+                        }
+                    }
+
+                    // if(!hasRoleAdmin)
+                    //     result = '';
+
+                    return result;
+                }
+            }],
+            translate: {
+                records: {
+                    processing: 'Подождите пожалуйста...',
+                    noRecords: 'Записей не найдено'
+                },
+                toolbar: {
+                    pagination: {
+                        items: {
+                            default: {
+                                first: 'Первая страница',
+                                prev: 'Предыдущая страница',
+                                next: 'Следующая страница',
+                                last: 'Последняя страница',
+                                more: 'Другие страницы',
+                                input: 'Номер страницы',
+                                select: 'Выбрать размер страницы'
+                            },
+                            info: 'Отображение {{start}} - {{end}} из {{total}} записей'
+                        }
+                    }
+                }
+            },
+
+            toolbar: {
+                items: {
+                    // pagination
+                    pagination: {
+                        // page size select
+                        pageSizeSelect: [5, 10, 20, 50, 100]
+                    }
+                }
+            }
+        });
+
+        datatable.sort("counter","asc");
+        var query = datatable.getDataSourceQuery();
+
+        $('#m_form_search3').on('keyup', function (e) {
+            datatable.search($(this).val().toLowerCase());
+        }).val(query.generalSearch);
+
+        /*
+        $('#m_form_status').on('change', function () {
+            datatable.search($(this).val(), 'Status');
+        }).val(typeof query.Status !== 'undefined' ? query.Status : '');
+
+        $('#m_form_type').on('change', function () {
+            datatable.search($(this).val(), 'Type');
+        }).val(typeof query.Type !== 'undefined' ? query.Type : '');
+
+        $('#m_form_status, #m_form_type').selectpicker();
+        */
+    };
+
+    return {
+        //== Public functions
+        init: function () {
+            // init dmeo
+            mainTableInit();
+        }
+    };
+}();
+
+jQuery(document).ready(function () {
+    DatatableDataLocalPayments.init();
+});
